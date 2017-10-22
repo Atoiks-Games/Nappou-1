99eb131684c3f535e9485bbabcad3905a7ae6328
Paul Teng <plankp@outlook.com>
2017-10-22 08:13:45 -0400

    Use opengl based game runner

--- src/main/java/org/atoiks/seihou/App.java
+++ src/main/java/org/atoiks/seihou/App.java
@@ -3,7 +3,7 @@ package org.atoiks.seihou;
 import com.ymcmp.jine.Game;
 import com.ymcmp.jine.Environment;
 import com.ymcmp.jine.GameRunner;
-import com.ymcmp.jine.environments.Java2D;
+import com.ymcmp.jine.environments.OpenGL;
 import com.ymcmp.jine.runners.FixedRateRunner;
 
 public class App {
@@ -13,7 +13,7 @@ public class App {
      */
     public static void main(String[] args) {
         final GameRunner runner = new FixedRateRunner();
-        final Environment env = new Java2D();
+        final Environment env = new OpenGL(false);
         final Game game = new ProjectSeihouGame();
 
         env.attachGame(game);
